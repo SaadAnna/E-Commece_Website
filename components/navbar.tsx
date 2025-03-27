b3ea605991cde908aba9f6ae62c9487a93d920a6
Saad Anna <saadannadev@gmail.com>
2025-03-27 15:54:28 +0100

    Start The Navbar Section In The Home Page

---
+++ components/navbar.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link"; 
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <Link href="/">Ecomre</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
