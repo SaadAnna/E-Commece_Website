0c5468860618f0640f73eb800714217c6708f029
Saad Anna <saadannadev@gmail.com>
2025-03-29 22:46:45 +0100

    Change the navbar content

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
   return (
     <nav>
       <div>
-        <Link href="/">My Ecommerce Website In 2025, Let&apos;s Do it Inshallah</Link>
+        <Link href="/">My ecommerce website</Link>
       </div>
     </nav>
   );
