5500050319bde669599290829c75affc06bd2077
Saad Anna <saadannadev@gmail.com>
2025-04-02 23:35:54 +0200

    Fix The Navbar Section

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
   return (
     <nav>
       <div>
-        <Link href="/">Hello, This Is My First E-commerce Website In 2025, Let&apos;s Do It</Link>
+        <Link href="/">Hello, This Is My First E-commerce Website In 2025, Let&apos;s Go</Link>
       </div>
     </nav>
   );
