bbf6d262be4383915c05c198abc9bc37d3b50cd5
Saad Anna <saadannadev@gmail.com>
2025-04-05 17:08:18 +0200

    Update the style in the navabar section and add stripe to the project

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,24 @@
-import Link from "next/link"; 
+import Link from "next/link";
 import React from "react";
 
 export const Navbar = () => {
   return (
-    <nav>
-      <div>
-        <Link href="/">Hello, This Is My First E-commerce Website In 2025, Let&apos;s Do It</Link>
+    <nav className="sticky top-0 z-50 bg-white shadow">
+      <div className="container mx-auto flex items-center justify-between px-4 py-4">
+        <Link href="/" className="text-white hover:text-blue-600">
+          Hello
+        </Link>
+
+        <div className="hidden md:flex space-x-6">
+          <Link href="/">Home</Link>
+          <Link href="/products" className="hover:text-blue-600">
+            Products
+          </Link>
+          <Link href="/checkout" className="hover:text-blue-600">
+            CheckOut
+          </Link>
+        </div>
+        <div className="flex items-center space-x-4"></div>
       </div>
     </nav>
   );
