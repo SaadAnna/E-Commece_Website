f149d6e4bd3defefcb10b3059a12f1a46656eeb0
Saad Anna <saadannadev@gmail.com>
2025-04-05 18:08:01 +0200

    Add the products images from stripe in the website

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,25 @@
 import { stripe } from "@/lib/stripe";
 import { Button } from "@/components/ui/button";
 import { Link } from "lucide-react";
+import Image from "next/image";
 export default async function Home() {
   const products = await stripe.products.list({
     expand: ["data.default_price"],
     limit: 5,
   });
-  console.log(products);
   return (
     <div>
       <section>
         <div>
-        <div>
-        
-        <h2>Welcome To Mesart</h2>
-        <p>Discover the latest products at the best price.</p>
-        <Button asChild variant="default">
-
-        <Link href="/products">Browse All Products</Link>
-        </Button>
+          <div>
+            <h2>Welcome To Mesart</h2>
+            <p>Discover the latest products at the best price.</p>
+            <Button asChild variant="default">
+              <Link href="/products">View All Products</Link>
+            </Button>
+          </div>
+          <Image alt="Banner Image" width={450} height={450} src={products.data[0].images[0]} />
         </div>
-        </div>
-        
       </section>
     </div>
   );
