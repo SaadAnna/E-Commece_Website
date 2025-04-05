98d5318837ec26e889699536528bcf6fc243a1b6
Saad Anna <saadannadev@gmail.com>
2025-04-05 17:28:33 +0200

    Add shadcn to the project for the home page button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
 import { stripe } from "@/lib/stripe";
+import { Button } from "@/components/ui/button";
+import { Link } from "lucide-react";
 export default async function Home() {
   const products = await stripe.products.list({
-  expand: ["data.default_price"],
-  limit: 5,
-  })
-  console.log(products)
-  return <div></div>;
+    expand: ["data.default_price"],
+    limit: 5,
+  });
+  console.log(products);
+  return (
+    <div>
+      <section>
+        <div>
+        <div>
+        
+        <h2>Welcome To Mesart</h2>
+        <p>Discover the latest products at the best price.</p>
+        <Button asChild variant="default">
+
+        <Link href="/products">Browse All Products</Link>
+        </Button>
+        </div>
+        </div>
+        
+      </section>
+    </div>
+  );
 }
