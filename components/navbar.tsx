707bee043712f0d80f053cc2b72b4266cac76af2
Saad Anna <saadannadev@gmail.com>
2025-04-07 21:40:36 +0200

    Change the Global and the navbar style for the project

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,26 +1,30 @@
-import Link from "next/link";
-import React from "react";
-
 export const Navbar = () => {
   return (
-    <nav className="sticky z-50 bg-white shadow mx-auto">
-      <div className="container mx-auto flex items-center justify-between px-4 py-20 ng-red-500">
-        <Link href="/" className="text-white hover:text-blue-600">
-          Hello
-        </Link>
-
-        <div className="hidden md:flex space-x-6">
-          <Link href="/">Home</Link>
-          <Link href="/products" className="hover:text-blue-600">
-            Products
-          </Link>
-          <Link href="/checkout" className="hover:text-blue-600">
-            CheckOut
-          </Link>
+    <header className="py-4 border-b border-black md:border-none sticky top-0 z-10 ">
+      <div className="absolute inset-0 backdrop-blur -z-10 md:hidden"></div>
+      <div className="container">
+        <div className="flex justify-between items-center md:border  border-black md:p-2.5 rounded-xl max-w-2xl lg:max-w-3xl mx-auto relative">
+          <div className="absolute inset-0 backdrop-blur -z-10 hidden md:block"></div>
+          <div>
+            <div className="border h-10 w-10 text-bold text-3xl rounded-lg inline-flex justify-center items-center border-black">
+              T
+            </div>
+          </div>
+          <div className="hidden md:block mx-auto"  id="navabar">
+            <nav className="flex gap-8 text-sm">
+              <a href="#" className="text-black text-base transition">
+                Home
+              </a>
+              <a href="#" className="text-black text-base transition">
+                Products
+              </a>
+             
+            </nav>
+          </div>
+         
         </div>
-        <div className="flex items-center space-x-4"></div>
       </div>
-    </nav>
+    </header>
   );
 };
 
