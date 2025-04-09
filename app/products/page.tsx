c198ad2ea0b5d96657e5f4a271112b0e50099531
Saad Anna <saadannadev@gmail.com>
2025-04-09 22:27:50 +0200

    Start the products page section

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,6 +1,15 @@
+import { ProductsList } from "@/components/products-list";
+import { stripe } from "@/lib/stripe";
 
-export default function ProductsPage ()  {
+export default async function ProductsPage() {
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
   return (
-    <div>ProductsPge</div>
-  )
+    <div>
+      <h1>All Products</h1>
+
+      <ProductsList products={products.data} />
+    </div>
+  );
 }
