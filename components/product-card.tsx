c19e726f211b2e1f6ed24240a762d0fd7415cecf
Saad Anna <saadannadev@gmail.com>
2025-04-09 22:34:01 +0200

    Add the product card for the product list in the product page

---
+++ components/product-card.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { Link } from "lucide-react";
+import Stripe from "stripe";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import Image from "next/image";
+import { Button } from "./ui/button";
+interface Props {
+  product: Stripe.Product;
+}
+export const ProductCard = ({ product }: Props) => {
+  const price = product.default_price as Stripe.Price;
+
+  return (
+    <Link href={"/products/1"}>
+      <Card>
+        {product.images && product.images[0] && (
+          <div className="relative  w-96 h-96 m-auto">
+            <Image
+              alt={product.name}
+              src={product.images[0]}
+              width={450}
+              height={450}
+              
+              objectFit="cover"
+              className="transition-opacity duration-500 ease-in-out"
+            />
+          </div>
+        )}
+
+        <CardHeader>
+          <CardTitle>{product.name}</CardTitle>
+          <CardContent>
+            {price && price.unit_amount && (
+              <p className="text-xl">${(price.unit_amount / 100).toFixed(2)}</p>
+            )}
+            <Button>View Details</Button>
+          </CardContent>
+        </CardHeader>
+      </Card>
+    </Link>
+  );
+};
