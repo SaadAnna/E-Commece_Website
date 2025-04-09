60b22d3c51b147f9bd377cacbbcd55044150bb35
Saad Anna <saadannadev@gmail.com>
2025-04-09 22:32:42 +0200

    Add the product list in the product page

---
+++ components/products-list.tsx
@@ -0,0 +1,25 @@
+import Stripe from "stripe";
+import { ProductCard } from "./product-card";
+
+interface Props {
+  products: Stripe.Product[];
+}
+export const ProductsList = ({ products }: Props) => {
+  return (
+    <div>
+      <div>
+        <input type="text" placeholder="Search Your Product..." />
+      </div>
+      <ul>
+        {products.map((product, key) => {
+          return (
+            <li key={key}>
+              <ProductCard product={product} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
