ef5e8ed6aaf08fec36a8190bf0d7ae52e9315bc9
Saad Anna <saadannadev@gmail.com>
2025-04-14 13:27:22 +0200

    Add some style to the product list

--- components/products-list.tsx
+++ components/products-list.tsx
@@ -7,10 +7,10 @@ interface Props {
 export const ProductsList = ({ products }: Props) => {
   return (
     <div>
-      <div>
-        <input type="text" placeholder="Search Product..." />
+      <div className="mb-14 flex justify-center">
+        <input type="text" placeholder="Search Product..."  className="w-full max-w-md px-4 p-2 rounded-lg py-2 border-solid  border border-gray-400 focus:outline-none focus:border-black"/>
       </div>
-      <ul>
+      <ul className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
         {products.map((product, key) => {
           return (
             <li key={key}>
