28c5acaf15f9c92de083baa8230aa06e9c69ef97
Saad Anna <saadannadev@gmail.com>
2025-04-16 14:02:02 +0200

    Add The Product Quantity In The Product Detail Page

--- components/product-detail.tsx
+++ components/product-detail.tsx
@@ -1,12 +1,26 @@
+"use client";
 import Stripe from "stripe";
 import Image from "next/image";
 import { Button } from "./ui/button";
+import { useCardStore } from "@/store/card-store";
 interface Props {
   product: Stripe.Product;
 }
 export const ProductDetail = ({ product }: Props) => {
+  const {items, addItem, removerItem} = useCardStore()
   const price = product.default_price as Stripe.Price;
+  const cartItem = items.find((item) => item.id === product.id);
+  const quantity = cartItem ? cartItem.quantity : 0;
 
+  const onAddItem = ()  => {
+    addItem({
+      id: product.id,
+      name: product.name,
+      price: price.unit_amount as number,
+      imageUrl: product.images ? product.images[0]: null,
+      quantity: 1,
+    })
+  }
   return (
     <div className="container mx-auto px-4 py-8 flex flex-col md:flex-row gap-8 items-center">
       {product.images && product.images[0] && (
@@ -30,10 +44,10 @@ export const ProductDetail = ({ product }: Props) => {
             <p className="text-xl text-black font-bold">${(price.unit_amount / 100).toFixed(2)}</p>
           )}
       <div className="flex items-center space-x-4 mt-2">
-      <Button className="bg-white text-black text-sm" variant={"outline"}>-</Button>
+      <Button onClick={() => removerItem(product.id) } className="bg-white text-black text-lg text-center cursor-pointer" variant={"outline"}>-</Button>
 
-       <span className="text-lg font-semibold">{0}</span>
-       <Button className="bg-black text-white text-sm" variant={"outline"}>+</Button>
+       <span className="text-lg font-semibold">{quantity}</span>
+       <Button onClick={onAddItem} className="bg-black text-white text-lg text-center hover:bg-black cursor-pointer hover:text-white" variant={"outline"}>+</Button>
 
       </div>
       </div>
