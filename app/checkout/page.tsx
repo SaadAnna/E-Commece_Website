557dd966040df84155dca20ac20a7b4c8f12ef77
Saad Anna <saadannadev@gmail.com>
2025-04-21 21:06:32 +0200

    start the checkout page in the project

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,6 +1,60 @@
+"use client";
 
-export default function CheckOutPage ()  {
-  return (
-    <div>CheckOutPage</div>
+import { useCardStore } from "@/store/card-store";
+import { Button } from "./ui/button";
+export default function CheckOutPage() {
+  const { items, removerItem, addItem } = useCardStore();
+  const total = items.reduce(
+    (acc, item) => acc + item.price + item.quantity, 
+    0
+  );
+
+  if (total === 0 || items.length === 0) {
+    return (
+<div>
+  <h1>Your Cart Is Empty</h1>
+</div>
+    );
+  }
+   return (
+    <div>
+    <h1>
+      Checkout
+    </h1>
+    <Card>
+
+    <CardHeader >
+
+    <CardTitle>
+      Order Summary
+    </CardTitle>
+    </CardHeader>
+    <CardContent>
+
+    <ul>
+      {items.map((item, key) => {
+       <li key={key}>
+
+       <div> 
+       <span>
+        {item.name}
+       </span>
+       <span>
+       ${(( item.price * item.quantity ) / 100 )toFixed(2)}
+       </span>
+       <div className="flex items-center space-x-4 mt-2">
+      <Button onClick={() => removerItem(item.id) } className="bg-white text-black text-lg text-center cursor-pointer" variant={"outline"}>-</Button>
+
+       <span className="text-lg font-semibold">{item.quantity}</span>
+       <Button onClick={() => addItem(item.id)} className="bg-black text-white text-lg text-center hover:bg-black cursor-pointer hover:text-white" variant={"outline"}>+</Button>
+
+      </div>
+       </div>
+       </li>
+      })}
+    </ul>
+    </CardContent>
+    </Card>
+    </div>
   )
 }
