fa113b4490792db8a16649ffd32249e7b6657b7e
Saad Anna <saadannadev@gmail.com>
2025-04-23 20:23:45 +0200

    change some content and update the checkout page

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useCardStore } from "@/store/card-store";
 import { Button } from "./ui/button";
 export default function CheckOutPage() {
@@ -18,39 +17,36 @@ export default function CheckOutPage() {
   }
    return (
     <div>
-    <h1>
+     <h1>
       Checkout
-    </h1>
+     </h1>
     <Card>
-
-    <CardHeader >
-
-    <CardTitle>
+      <CardHeader >
+        <CardTitle>
        Your Order Summary
-    </CardTitle>
-    </CardHeader>
+        </CardTitle>
+       </CardHeader>
+      
     <CardContent>
-
     <ul>
       {items.map((item, key) => {
        <li key={key}>
 
-       <div> 
-       <span>
-        {item.name}
-       </span>
-       <span>
-       ${(( item.price * item.quantity ) / 100 )toFixed(2)}
-       </span>
+         <div> 
+          <span>
+            {item.name}
+          </span>
+          <span>
+            ${(( item.price * item.quantity ) / 100 )toFixed(2)}
+          </span>
+
        <div className="flex items-center space-x-4 mt-2">
-      <Button onClick={() => removerItem(item.id) } className="bg-white text-black text-lg text-center cursor-pointer" variant={"outline"}>-</Button>
-
-       <span className="text-lg font-semibold">{item.quantity}</span>
-       <Button onClick={() => addItem(item.id)} className="bg-black text-white text-lg text-center hover:bg-black cursor-pointer hover:text-white" variant={"outline"}>+</Button>
-
-      </div>
+        <Button onClick={() => removerItem(item.id) } className="bg-white text-black text-lg text-center cursor-pointer" variant={"outline"}>-</Button>
+          <span className="text-lg font-semibold">{item.quantity}</span>
+        <Button onClick={() => addItem(item.id)} className="bg-black text-white text-lg text-center hover:bg-black cursor-pointer hover:text-white" variant={"outline"}>+</Button>
        </div>
-       </li>
+     </div>
+    </li>
       })}
     </ul>
     </CardContent>
