442b53bcf81fe7e32f0426122e51eba3921b7028
Saad Anna <saadannadev@gmail.com>
2025-04-30 21:32:40 +0200

    Change some style in the navbar section in the project

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -44,7 +44,7 @@ export const Navbar = () => {
           <Link href="/checkout" className="relative">
             <ShoppingCartIcon className="h-6 w-6" />
             {cartCount > 0 && (
-              <span className="absolute -top-2 -right-2 flex h-5 w-5 items-center justify-center rounded-full bg-red-500 text-xs text-white">
+              <span className="absolute -top-2 -right-2 flex h-5 w-5 items-center justify-center rounded-full bg-red-600  text-xs text-white">
                 {cartCount}
               </span>
             )}
