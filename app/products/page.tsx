7626067d3c3622a4ad4fa17a4c29f6fe813fa261
Saad Anna <saadannadev@gmail.com>
2025-05-01 17:02:38 +0200

    Add some style in the product page in the project

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -6,7 +6,7 @@ export default async function ProductsPage() {
     expand: ["data.default_price"],
   });
   return (
-    <div className="pb-8">
+    <div className="pb-10">
       <h1 className="lg:text-5xl text-3xl font-bold leading-none tracking-tight text-foreground text-center mb-8">All Products</h1>
 
       <ProductsList products={products.data} />
