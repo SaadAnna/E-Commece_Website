4a814d8b9a6c8034736a88827276d0aee3cc2d13
Saad Anna <saadannadev@gmail.com>
2025-05-05 15:37:30 +0200

    Add the checkout action page for the payment medthod

---
+++ app/checkout/checkout-action.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import { stripe } from "@/lib/stripe";
+import { CardItem } from "@/store/card-store";
+import { redirect } from "next/navigation";
+export const checkoutAction = async (formData: FormData): Promise<void> => {
+  const itemsJson = formData.get("items") as string;
+  const items = JSON.parse(itemsJson);
+  const line_items = items.map((item: CardItem) => ({
+    price_data: {
+      currency: "cad",
+      product_detail: { name: item.name },
+      unit_amount: item.price,
+    },
+    quantity: item.quantity,
+  }));
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card", "paypal"],
+    line_items,
+    mode: "payment",
+    success_url: `${process.env.NEXT_PUPLIC_BASE_URL}/`,
+    cancel_url: `${process.env.NEXT_PUPLIC_BASE_URL}/checkout`,
+  });
+  redirect(session.url!);
+};
