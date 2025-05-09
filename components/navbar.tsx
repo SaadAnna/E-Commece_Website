6423136787a3e5f19ea44832e45cd640c38bdd43
Saad Anna <saadannadev@gmail.com>
2025-05-09 11:28:35 +0200

    Change some style in the navbar section

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -28,8 +28,8 @@ export const Navbar = () => {
   return (
     <nav className="sticky top-0 z-50 bg-white shadow">
       <div className="container mx-auto flex items-center justify-between px-4 py-4">
-        <Link href="/" className="hover:text-blue-600">
-          E-Commerce
+        <Link href="/" className="hover:text-blue-600 text-black font-bold">
+          Gersmen
         </Link>
         <div className="hidden md:flex space-x-6">
           <Link href="/">Home</Link>
