977df437b3b6f9490eba523d9f3616c5e16b914d
Saad Anna <saadannadev@gmail.com>
2025-05-09 11:27:44 +0200

    Add the success page for the payment page

---
+++ app/success/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useCardStore } from "@/store/card-store";
+import Link from "next/link";
+import { useEffect } from "react";
+
+export default function SucessPage() {
+  const { clearCart } = useCardStore();
+  useEffect(() => {
+    clearCart();
+  }, [clearCart]);
+  return (
+    <div className="flex justify-center mx-auto mt-14 flex-col text-center gap-3">
+      <h1 className="text-bold text-3xl text-black">Payment Seccessufull.</h1>
+      <p className="tex-lg text-gray-500 text-medium">
+        Thank You For Chossing Our Store, Your Order Is Being Processed.
+      </p>
+      <Button>
+        <Link href={"/products"}>Continue Shopping</Link>
+      </Button>
+    </div>
+  );
+}
