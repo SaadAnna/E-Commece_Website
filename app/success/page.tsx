d956f7c2a3525283ace25be0d6bf2265d0f2f47f
Saad Anna <saadannadev@gmail.com>
2025-05-10 14:59:59 +0200

    Change some content in the home page

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -11,11 +11,11 @@ export default function SucessPage() {
   }, [clearCart]);
   return (
     <div className="flex justify-center mx-auto mt-14 flex-col text-center gap-3">
-      <h1 className="text-bold text-3xl text-black">Payment Seccessufull.</h1>
+      <h1 className="text-bold text-3xl text-green-600">Payment Seccessufull.</h1>
       <p className="tex-lg text-gray-500 text-medium">
         Thank You For Chossing Our Store, Your Order Is Being Processed.
       </p>
-      <Button>
+      <Button className="w-max-3xl mx-auto">
         <Link href={"/products"}>Continue Shopping</Link>
       </Button>
     </div>
